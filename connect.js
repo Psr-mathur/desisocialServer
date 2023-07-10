7f17f3acd2e4489db92804c0905ce1c07391e574
Prakash Kumar <Prakashkumarggh@gmail.com>
2023-07-10 20:15:59 +0530

    ready with setup

--- connect.js
+++ connect.js
@@ -1,22 +1,22 @@
 import mysql from "mysql";
 import "dotenv/config";
 
-export const db = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "744542",
-	database: "social",
-});
+// export const db = mysql.createConnection({
+// 	host: "localhost",
+// 	user: "root",
+// 	password: "744542",
+// 	database: "social",
+// });
 // export const db = mysql.createConnection(process.env.DATABASE_URL);
 // export const db = mysql.createConnection(process.env.DATABASE_URL_CLEVERCLOUD);
 
-// export const db = mysql.createConnection({
-//     // working
-//     host: "db4free.net",
-//     password: "desisocialpassword",
-//     user: "desisocialuser",
-//     database: "desisocial",
-// });
+export const db = mysql.createConnection({
+	// working
+	host: "db4free.net",
+	password: "desisocialpassword",
+	user: "desisocialuser",
+	database: "desisocial",
+});
 // export const db = mysql.createConnection({
 //     // working
 //     host: "b4ifp7riy7rrm641xnia-mysql.services.clever-cloud.com",
